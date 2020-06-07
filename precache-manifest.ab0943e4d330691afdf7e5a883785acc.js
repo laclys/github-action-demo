5daa65e89f7bceb5e12250074be94b3c9c83f884
laclys <laclys@126.com>
2020-06-07 08:14:26 +0000

    Deploying to gh-pages from master 047832c726fb0e928e981d654672bc305696f260

---
+++ precache-manifest.ab0943e4d330691afdf7e5a883785acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4182cd404aa01c74a0f401cf32f0bfe3",
+    "url": "/github-action-demo/index.html"
+  },
+  {
+    "revision": "3eff1c41a9e03f5686e3",
+    "url": "/github-action-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f00c9535ee2733f80d60",
+    "url": "/github-action-demo/static/js/2.29d9885a.chunk.js"
+  },
+  {
+    "revision": "3eff1c41a9e03f5686e3",
+    "url": "/github-action-demo/static/js/main.71948edc.chunk.js"
+  },
+  {
+    "revision": "c0fc3775537889a501e5",
+    "url": "/github-action-demo/static/js/runtime-main.58d15606.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-action-demo/static/media/logo.25bf045c.svg"
+  }
+]);
